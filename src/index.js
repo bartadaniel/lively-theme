18044ebf92db250a62bf8954b74031935212a8e6
Daniel Barta <daniel.barta@i-code.com>
2020-01-29 15:59:40 +0100

    rename plugin in index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 module.exports = {
   getThemes () {
     return {
-      'plugin-light-mode': {
+      'plugin-lively-mode': {
         cssPath: 'styles/light.css',
         darkMode: false
       }
